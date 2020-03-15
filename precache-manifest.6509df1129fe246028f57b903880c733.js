f363cf6dc2364171495b04c73195068b358430c4
miguel-miranda <ayandram7727@gmail.com>
2020-03-15 04:01:44 -0300

    Updates

---
+++ precache-manifest.6509df1129fe246028f57b903880c733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58179f675e6d3ab18510fc7b6542a2c2",
+    "url": "/talentos-digitais-localiza/index.html"
+  },
+  {
+    "revision": "706da66ab91659f9e045",
+    "url": "/talentos-digitais-localiza/static/css/main.346c23a1.chunk.css"
+  },
+  {
+    "revision": "e5e6353d3b2b8dec97a4",
+    "url": "/talentos-digitais-localiza/static/js/2.29f98829.chunk.js"
+  },
+  {
+    "revision": "38d1f7573e3f8e03f12894c14c5352ff",
+    "url": "/talentos-digitais-localiza/static/js/2.29f98829.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706da66ab91659f9e045",
+    "url": "/talentos-digitais-localiza/static/js/main.2e1050c7.chunk.js"
+  },
+  {
+    "revision": "a6a02c1eb02aaafe561d",
+    "url": "/talentos-digitais-localiza/static/js/runtime-main.82dd5dcc.js"
+  },
+  {
+    "revision": "1f6bc3df4d32bb0132607092a9b4aa0f",
+    "url": "/talentos-digitais-localiza/static/media/estrela-da-morte.1f6bc3df.jpg"
+  }
+]);
